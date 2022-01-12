01e78aa7bee5aacdedb4de6d8286365e52e5e759
ProfLeao <prof.reginaldo.leao@gmail.com>
2022-01-12 17:59:23 -0300

    Avancos na funcao statusINMET

--- SolarPowerForecastingIFMG/RaspagemDeDadosINMET.jl
+++ SolarPowerForecastingIFMG/RaspagemDeDadosINMET.jl
@@ -8,9 +8,22 @@ function statusINMET()
 
     resposta = HTTP.request("GET", ENDERECO)
     dados_anos = Gumbo.parsehtml(String(resposta.body))
+    return dados_anos
+    # Extrai as tags <a xxxx> xxxx </a>
+    idx = 1
+    list = []
+    #for tag_a in dados_anos.root[2][4][1][1] # O método de indexação está frágil
+    #    if idx%2 == 0
+    #        push!(
+    #            list, 
+    #            split(tag_a[1][1].text)
+    #        )
+    #    end
+    #    idx+=1
+    #end
+    #intervalo_de_dados = (prim_ano = list[1], ult_ano = list[end])
 
-    # Extrai as tags <a xxxx> xxxx </a> 
-    dados_anos = dados_anos.root[2][4][1][1][2:2:end]
+    #return intervalo_de_dados
 end
 
 resp = statusINMET()
